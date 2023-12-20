4b2392301b5df856eb434a589692025e0a98c392
Resn4R <purview-joust.0x@icloud.com>
2023-12-20 10:02:25 +0000

    added message of the day through jinja2

--- flask tests.py
+++ flask tests.py
@@ -1,11 +1,15 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    # Define a dynamic message based on the current day of the week
+    day_of_week = datetime.datetime.now().strftime("%A")
+    messageOfTheDay = f"Happy {day_of_week}!"
+    
+    return render_template("home.html",message = messageOfTheDay)
 
 @app.route('/about')
 def about():
